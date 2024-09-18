297d5448835a9f64dcabb9d9773d8cc720b1eef7
Andres Murillo <andresmimi@gmail.com>
2024-09-17 23:17:09 -0600

    Se deja codigo trabajado en clase como oficial, y se eliminan carpetas vacias

--- src/SistemaArchivosFAT.hpp
+++ src/SistemaArchivosFAT.hpp
@@ -5,60 +5,53 @@
 #include <vector>
 
 // Definiciones básicas
-const int TAM_BLOQUE = 8;     // Tamaño de cada bloque
-const int TAM_UNIDAD = 4096;  // Tamaño de la unidad (arreglo de 4096 chars)
+const int TAM_BLOQUE = 8;               // Tamaño de cada bloque
+const int TAM_UNIDAD = 4096;            // Tamaño de la unidad (arreglo de 4096 chars)
 const int TAM_TABLA = 1 << TAM_BLOQUE;  // tamaño de FAT es 2^8
 
 // Estructura para representar un archivo en el sistema
 struct Archivo {
   std::string nombre;
-  int bloqueInicio;  // Bloque donde comienza el archivo:vector<char> datos1 = {'H', 'o', 'l', 'a', 'M', 'u', 'n', 'd', 'o'};
+  int bloqueInicio;
   int bloqueFin;
-  // int tamaño;  // Tamaño del archivo en bloques, no se si esto realmente es
-  // asi, quiza despues se cambie inicializar el archivo
-  bool abierto;  // Nueva bandera para indicar si el archivo está abierto
+  bool abierto;
   std::string permisos;
   std::string usuario;
   int proceso;
-  Archivo() : nombre(""), bloqueInicio(-1), bloqueFin(-1), abierto(false), permisos ("r--"), usuario(""), proceso(-1){}
+  int cursor; // para saber de donde desea empezar a leer
+  Archivo() : nombre(""), bloqueInicio(-1), bloqueFin(-1), abierto(false),
+    permisos ("r--"), usuario(""), proceso(-1), cursor(0){}
 };
 
 struct Marcos {
-  char marco[TAM_BLOQUE];  // Cada frame tiene 8 bytes
+  char marco[TAM_BLOQUE]; // Cada frame tiene 8 bytes
 };
 
 // Clase SistemaArchivosFAT
 class SistemaArchivosFAT {
  private:
+  int cantidadDirectorios;
   Marcos unidad[TAM_UNIDAD / TAM_BLOQUE];  // Arreglo de frames
   int tablaBloques[TAM_TABLA];      // Tabla de bloques de 8 chars cada uno
-  std::vector<Archivo> directorio;  // Directorio de archivos
-
+  //std::vector<Archivo> directorio;  // Directorio de archivos
+  Archivo directorio [TAM_UNIDAD];
+  // Método auxiliar para buscar archivos
+  int encontrarArchivo(const std::string& nombreArchivo);
+  void escribirA(const std::string& nombreArchivo, std::vector<char>& datos);
  public:
   SistemaArchivosFAT();
-
-  // Métodos del sistema de archivos
-  void abrir(const std::string& nombreArchivo);
+  void abrir(const std::string& nombreArchivo, bool verbose);
   void cerrar(const std::string& nombreArchivo);
-  void crear(const std::string& nombreArchivo, std::vector<char>& datos);
+  void crear(const std::string& nombreArchivo);
   void leer(const std::string& nombreArchivo);
   void buscar(const std::string& nombreArchivo);
   void borrar(const std::string& nombreArchivo);
   void adjuntar(const std::string& nombreArchivo, std::vector<char>& datos);
   void renombrar(const std::string& nombreArchivo,
-                 const std::string& nuevoNombre);
+    const std::string& nuevoNombre);
   void listar();
-
-
- private:
-  // Método auxiliar para buscar archivos
-  int encontrarArchivo(const std::string& nombreArchivo);
-  // Método auxiliar para asignar bloques
-  int asignarBloques(int tamaño);
-  // Método auxiliar para liberar bloques
-  void liberarBloques(int bloqueInicio);
-  // Método auxiliar para escribir datos
-  void escribir(size_t marco, std::vector<char>& datos);
+  void escribir(const std::string& nombreArchivo, std::vector<char>& datos);
+  void imprimir();
 };
 
 #endif  // SISTEMA_ARCHIVOS_FAT_HPP
